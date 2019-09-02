3a00b353760ea1ae77c2ab06b5328f249ec6c6c0
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-02 15:08:30 +0100

    Add route to cat

--- app.rb
+++ app.rb
@@ -9,3 +9,9 @@ end
 get '/secret' do
   "shhhh - don't tell anyone - it's a secret"
 end
+
+get '/cat' do
+  "<div style='border: 4px dashed green'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
